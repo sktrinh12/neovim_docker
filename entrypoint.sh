aa198de467eef6de5d8f84db335ea77f8c213f3d
sktrinh12 <trinh.kcs85@gmail.com>
2020-06-22 01:15:17 -0400

    refactor: condensed a one-liner for the su-exec
    Condensed the two lines of code to one which is just combines the two
    commands which is a PlugUpdate and open terminal

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,8 @@
 
 #usermod -u "${UID}" neovim
 #groupmod -g "${GID}" neovim
-#su-exec neovim nvim +PlugInstall +qall 
 cd "${WORKSPACE}" && \
 	# su-exec neovim nvim --cmd 'set shada+=/home/neovim/.local/share/nvim/main.shada' +UpdateRemotePlugins +qall && \
-	su-exec neovim nvim +UpdateRemotePlugins +qall && \
-	su-exec neovim nvim --cmd 'split | te python3' "$@"
+	su-exec neovim nvim +UpdateRemotePlugins +PlugUpdate \ #+qall && \
+    +'split | te python3' "$@"
 
